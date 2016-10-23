62f81f47dc2f63c3b21fbd8826d0f0ea8cf1837a
gotrecillo <sergio.panadero.perez@gmail.com>
2016-10-23 23:22:22 +0200

    added form builder

--- resources/views/flyers/create.blade.php
+++ resources/views/flyers/create.blade.php
@@ -1,5 +1,22 @@
 @extends('layout')
 
 @section('content')
-    <h1>Selling your home ?</h1>
+    <h1>Selling your Home ?</h1>
+
+    <form action="">
+        <div class="field"><!-- Street Form Input-->
+            {{ Form::label('street', 'Street') }}
+            {{ Form::text('street', null) }}
+        </div>
+        
+        <div class="field"><!-- City Form Input-->
+            {{ Form::label('city', 'City') }}
+            {{ Form::text('city', null) }}
+        </div>
+        
+        <div class="field"><!-- Zip Form Input-->
+            {{ Form::label('zip', 'Zip/Postal Code') }}
+            {{ Form::text('zip', null) }}
+        </div>
+    </form>
 @stop
